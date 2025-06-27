7a17afd75a9f30cdde907b923e12aafbf8726564
FeerGonzalez <fernando.g.gonzalez@hotmail.com>
2025-06-27 19:58:03 -0300

    Inicio TpMongo
    Subida del proyecto de Back del tp de MongoDB al repositorio BD2

---
+++ BD2 - TpMongo/src/main/java/Tp_Mongo/Tp_Mongo/TpMongoApplication.java
@@ -0,0 +1,13 @@
+package Tp_Mongo.Tp_Mongo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TpMongoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TpMongoApplication.class, args);
+	}
+
+}
---
+++ BD2 - TpMongo/src/test/java/Tp_Mongo/Tp_Mongo/TpMongoApplicationTests.java
@@ -0,0 +1,13 @@
+package Tp_Mongo.Tp_Mongo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TpMongoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
